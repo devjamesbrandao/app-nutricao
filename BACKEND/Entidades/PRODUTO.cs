382aa78d529f6c6b4dee88723042dbfb8024143a
James Brandao <jamesbrandao@radinfo.com.br>
2022-02-08 08:02:01 -0300

    Entidades

---
+++ BACKEND/Entidades/PRODUTO.cs
@@ -0,0 +1,10 @@
+﻿
+namespace BACKEND.Entidades
+{
+    public partial class PRODUTO
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string CodBarra { get; set; }
+    }
+}
---
+++ BACKEND/Entidades/USUARIO_RESTRICAO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BACKEND.Entidades
+{
+    public partial class USUARIO_RESTRICAO
+    {
+        public int FK_RESTRICAO_ALIMENTAR_ID { get; set; }
+        public int FK_USUARIO_ID { get; set; }
+
+        public virtual RESTRICAO_ALIMENTAR FK_RESTRICAO_ALIMENTAR { get; set; }
+        public virtual USUARIO FK_USUARIO { get; set; }
+    }
+}
