2b92a3c4be39c9d801c703b5e5093203bb0eb834
James Brandao <jamesbrandao@radinfo.com.br>
2022-02-09 07:32:54 -0300

    Contexto

--- BACKEND/Context/NutricaoContext.cs
+++ BACKEND/Context/NutricaoContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using BACKEND.Entidades;
+﻿using BACKEND.Entidades;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
 
-namespace BACKEND.Scaffold
+namespace BACKEND.Context
 {
     public partial class NutricaoContext : DbContext
     {
@@ -27,102 +24,7 @@ namespace BACKEND.Scaffold
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<PRODUTO>(entity =>
-            {
-                entity.ToTable("PRODUTO");
-
-                entity.Property(e => e.ID).ValueGeneratedNever();
-
-                entity.Property(e => e.CodBarra)
-                    .HasMaxLength(20)
-                    .IsUnicode(false);
-
-                entity.Property(e => e.Nome)
-                    .HasMaxLength(250)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<PRODUTO_INGREDIENTE>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToTable("PRODUTO_INGREDIENTE");
-
-                entity.HasOne(d => d.FK_INGREDIENTE)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_INGREDIENTE_ID)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_PRODUTO_INGREDIENTE_2");
-
-                entity.HasOne(d => d.FK_PRODUTO)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_PRODUTO_ID)
-                    .HasConstraintName("FK_PRODUTO_INGREDIENTE_1");
-            });
-
-            modelBuilder.Entity<RESTRICAO_ALIMENTAR>(entity =>
-            {
-                entity.ToTable("RESTRICAO_ALIMENTAR");
-
-                entity.Property(e => e.ID).ValueGeneratedNever();
-
-                entity.Property(e => e.Descricao)
-                    .HasMaxLength(250)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<RESTRICAO_INGREDIENTE>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToTable("RESTRICAO_INGREDIENTE");
-
-                entity.HasOne(d => d.FK_INGREDIENTE)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_INGREDIENTE_ID)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_RESTRICAO_INGREDIENTE_1");
-
-                entity.HasOne(d => d.FK_RESTRICAO_ALIMENTAR)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_RESTRICAO_ALIMENTAR_ID)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_RESTRICAO_INGREDIENTE_2");
-            });
-
-            modelBuilder.Entity<USUARIO>(entity =>
-            {
-                entity.ToTable("USUARIO");
-
-                entity.Property(e => e.ID).ValueGeneratedNever();
-
-                entity.Property(e => e.Nome)
-                    .HasMaxLength(250)
-                    .IsUnicode(false);
-            });
-
-            modelBuilder.Entity<USUARIO_RESTRICAO>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToTable("USUARIO_RESTRICAO");
-
-                entity.HasOne(d => d.FK_RESTRICAO_ALIMENTAR)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_RESTRICAO_ALIMENTAR_ID)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_USUARIO_RESTRICAO_1");
-
-                entity.HasOne(d => d.FK_USUARIO)
-                    .WithMany()
-                    .HasForeignKey(d => d.FK_USUARIO_ID)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK_USUARIO_RESTRICAO_2");
-            });
-
-            OnModelCreatingPartial(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NutricaoContext).Assembly);
         }
-
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
