05ee28bcc311e4a7ebd23fa55025e252dba21580
James Brandao <jamesbrandao@radinfo.com.br>
2022-02-09 07:32:47 -0300

    Dependências

--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -1,3 +1,6 @@
+using BACKEND.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,9 +10,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors();
+
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<NutricaoContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("Conexao")
+));
+
 var app = builder.Build();
+
+app.UseCors(
+    x => x.SetIsOriginAllowed(x => true)
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .AllowAnyMethod()
+);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
