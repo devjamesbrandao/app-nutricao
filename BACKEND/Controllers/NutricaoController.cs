4db829fd0cd70f442729ad58a2393a30c8824d6a
James Noleto <muricijames@gmail.com>
2022-02-13 16:59:18 -0300

    refactor: alteração de retorno

--- BACKEND/Controllers/NutricaoController.cs
+++ BACKEND/Controllers/NutricaoController.cs
@@ -21,17 +21,17 @@ public class AppNutricaoController : ControllerBase
     [HttpGet("verificar-consumo")]
     public async Task<IActionResult> VerificarConsumo([FromQuery] string CodBarra, int CodUsuario)
     {
-        return Ok(new Dados { Produtos = new List<Produto>{
-            new Produto {
-                CodBarras = "123",
-                Descricao = "Bolacha",
-                IdProduto = "147852",
-                Marca = "Club Social",
-                Titulo = "Biscoito de sal",
-                UrlImagem = @"",
-                Ingredientes = new List<string>{"Farinha de trigo"}
-            }
-        }});
+        return Ok(new List<Produto>{
+                    new Produto {
+                        CodBarras = "123",
+                        Descricao = "Bolacha",
+                        IdProduto = "147852",
+                        Marca = "Club Social",
+                        Titulo = "Biscoito de sal",
+                        UrlImagem = @"http://127.0.0.1:8887/produtoAmendoimBrasil.png",
+                        Ingredientes = new List<string>{"Farinha de trigo"}
+                    }
+                });
         // return Ok(await _nutriService.VerificarCosumoDeProdutoPorCodUsuario(CodUsuario, CodBarra));
     }
 }
