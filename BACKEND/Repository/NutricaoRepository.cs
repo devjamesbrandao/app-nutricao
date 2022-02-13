d4421aa6c86b9f0f981bf02506c0118f2f1ffe20
James Noleto <muricijames@gmail.com>
2022-02-13 16:23:53 -0300

    refactor: retornar dados do produto

--- BACKEND/Repository/NutricaoRepository.cs
+++ BACKEND/Repository/NutricaoRepository.cs
@@ -13,16 +13,22 @@ namespace BACKEND.Repository
             _context = context;
         }
 
+        /*
+        SELECT I.Descricao FROM USUARIO U
+        INNER JOIN USUARIO_RESTRICAO UR ON U.ID = UR.FK_USUARIO_ID
+        INNER JOIN RESTRICAO_ALIMENTAR RA ON UR.FK_RESTRICAO_ALIMENTAR_ID = RA.ID
+        INNER JOIN RESTRICAO_INGREDIENTE RI ON RA.ID = RI.FK_RESTRICAO_ALIMENTAR_ID
+        INNER JOIN INGREDIENTE I ON RI.FK_INGREDIENTE_ID = I.ID
+        INNER JOIN PRODUTO_INGREDIENTE PI ON I.ID = PI.FK_INGREDIENTE_ID
+        INNER JOIN PRODUTO P ON PI.FK_PRODUTO_ID = P.ID
+        WHERE P.CodBarra = '7898594710534' AND U.ID = 1
+        */
+
         public async Task<IEnumerable<string>> VerificarCosumoDeProdutoPorCodUsuario(
-            int CodUsuario, int CodProd
+            int CodUsuario, string CodBarra
         )
         {
-            return await (
-                from u in _context.USUARIOs
-                join ur in _context.USUARIO_RESTRICAOs
-                on u.ID equals ur.FK_USUARIO_ID
-                select u.Nome
-            ).ToListAsync();
+            return new List<string>{"TEste"};
         }
     }
 }
