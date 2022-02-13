465a87b20d8a95d983bf06ff7d82568428da40f3
James Noleto <muricijames@gmail.com>
2022-02-13 16:21:22 -0300

    refactor: alteração parâmetro de busca

--- BACKEND/Interfaces/INutricaoRepository.cs
+++ BACKEND/Interfaces/INutricaoRepository.cs
@@ -2,6 +2,6 @@ namespace BACKEND.Interfaces
 {
     public interface INutricaoRepository
     {
-        Task<IEnumerable<string>> VerificarCosumoDeProdutoPorCodUsuario(int CodUsuario, int CodProd);
+        Task<IEnumerable<string>> VerificarCosumoDeProdutoPorCodUsuario(int CodUsuario, string CodBarra);
     }
 }
